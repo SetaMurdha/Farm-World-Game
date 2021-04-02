4219affa814145ffbb93671a7240c306681a9f98
Seta Murdha <murdhapamungkas@gmail.com>
2021-04-02 19:27:33 +0700

    ---

--- Farm World/Assets/Script/TimeScript.cs
+++ Farm World/Assets/Script/TimeScript.cs
@@ -6,22 +6,39 @@ using UnityEngine.UI;
 public class TimeScript : MonoBehaviour
 {
     // Start is called before the first frame update
-    public float timeRemaining = 10f;
+    public float timeRemaining = 10;
     public GameObject panelLost;
     public Text timeText;
-    
+
+    private void Start()
+    {
+
+    }
+
     // Update is called once per frame
     void Update()
     {
-        if(timeRemaining > 0)
+        
+        if (timeRemaining > 1)
         {
             timeRemaining -= Time.deltaTime;
             Debug.Log(timeRemaining);
+            DisplayTime(timeRemaining);
         }
         else
         {
             panelLost.SetActive(true);
             Time.timeScale = 0f;
         }
+        
     }
+
+    void DisplayTime(float timeToDisplay)
+    {
+        float minutes = Mathf.FloorToInt(timeToDisplay / 60);
+        float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+
+        timeText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+
 }
