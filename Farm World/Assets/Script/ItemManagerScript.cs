77f4883c645053875969a0af111482d736eca413
Seta Murdha <murdhapamungkas@gmail.com>
2021-04-06 09:55:36 +0700

    ---

--- Farm World/Assets/Script/ItemManagerScript.cs
+++ Farm World/Assets/Script/ItemManagerScript.cs
@@ -32,7 +32,7 @@ public class ItemManagerScript : MonoBehaviour
 
     public void gameLevel1()
     {
-        if(meat == 5)
+        if(meat == 10 && spinach == 5)
         {
             WinPanel.SetActive(true);
             Time.timeScale = 0f;
