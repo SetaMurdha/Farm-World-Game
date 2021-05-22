7bb4370df0a7cc2103b0e03504e0c6805c436f89
Seta Murdha <murdhapamungkas@gmail.com>
2021-05-22 20:39:45 +0700

    ---

---
+++ Farm World/Assets/Script/playerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class playerController : MonoBehaviour,IPointerDownHandler,IPointerUpHandler, IDragHandler
+{
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = new Vector2(eventData.position.x, transform.position.y);
+
+        if(transform.localPosition.x < -80)
+        {
+            transform.localPosition = new Vector2(-80, 0);
+        }
+        if(transform.localPosition.x > 80)
+        {
+            transform.localPosition = new Vector2(80, 0);
+        }
+
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        transform.localPosition = Vector2.zero;
+    }
+    
+}
